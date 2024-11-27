94df065bac34e848961e7d704aa67c03906ccc6b
mohammad abdul azeem <azeemaptech724@gmail.com>
2024-11-27 10:11:36 +0500

    new adding

---
+++ import export/pro.js
@@ -0,0 +1,17 @@
+  // Named exports
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+
+// Default export
+export default function multiply(a, b) {
+  return a * b;
+}
+
+
+
+
+// // greet.js
+// export default function greet(name) {
+//     return `Hello, ${name}!`;
+//   }
+  
